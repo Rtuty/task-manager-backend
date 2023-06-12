ed93a32fd74e37eb0c37f8d7dfd1eca18a725750
Kudryavtsev Kirill <rtutiui@mail.ru>
2023-06-12 23:44:46 +0300

    add createTable, collectData and getNumberOfUsers

--- internal/db/postgres.go
+++ internal/db/postgres.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"database/sql"
 	"fmt"
 	"os"
 	"reflect"
@@ -35,9 +36,63 @@ func GetConnection() {
 
 var dbConStr string = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", PstgCon.Host, PstgCon.Port, PstgCon.User, PstgCon.Passwd, PstgCon.Dbname, PstgCon.Sslmode)
 
-// func createTable() error {
-// 	db, err := sql.Open("postgres", dbConStr)
-// 	if err != nil {
-// 		return err
-// 	}
-// }
+func createTable() error {
+	db, err := sql.Open("postgres", dbConStr)
+	if err != nil {
+		return err
+	}
+
+	defer db.Close()
+
+	//Создаем таблицу users
+	if _, err = db.Exec(`
+		CREATE TABLE users(
+			ID SERIAL PRIMARY KEY,
+			TIMESTAMP TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+			USERNAME TEXT,
+			CHAT_ID INT,
+			MESSAGE TEXT,
+			ANSWER TEXT);`); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Данные по пользователям из бота записываем в БД
+func collectData(username string, chatid int64, message string, answer []string) error {
+	db, err := sql.Open("postgres", dbConStr)
+	if err != nil {
+		return err
+	}
+
+	defer db.Close()
+
+	answ := strings.Join(answer, ", ")
+	data := `INSERT INTO users(username, chatid, message, answer) values ($1, $2, $3, $4)`
+
+	if _, err := db.Exec(data, `@`+username, chatid, message, answ); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getNumberOfUsers() (int64, error) {
+	var count int64
+
+	db, err := sql.Open("postgres", dbConStr)
+	if err != nil {
+		return 0, err
+	}
+	defer db.Close()
+
+	//Отправляем запрос в БД для подсчета числа уникальных пользователей
+	row := db.QueryRow("SELECT COUNT(DISTINCT username) FROM users;")
+	err = row.Scan(&count)
+	if err != nil {
+		return 0, err
+	}
+
+	return count, nil
+}
